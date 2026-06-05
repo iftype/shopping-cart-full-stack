c7272640b376b9947afac7b9b2f50549905ee281
iftype <conditionaltype@gmail.com>
2026-06-05 15:59:10 +0900

    feat: 주문 금액 요약과 제출 버튼을 가진 CartSummary 컴포넌트 구현

--- client/src/features/cart-section/CartSummary.tsx
+++ client/src/features/cart-section/CartSummary.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import {
   calculateOrderPrice,
   calculateShippingFee,
@@ -5,15 +6,32 @@ import {
   type CartItem,
 } from "../../entites/cart/model";
 
-export const CartSummary = ({ cartItems }: { cartItems: CartItem[] }) => {
-  const orderPrice = calculateOrderPrice(cartItems);
+export interface CartSummaryProps {
+  checkedItems: CartItem[];
+}
+
+export const CartSummary = ({ checkedItems }: CartSummaryProps) => {
+  const navigate = useNavigate();
+  const orderPrice = calculateOrderPrice(checkedItems);
   const shippingFee = calculateShippingFee(orderPrice);
   const totalOrderPrice = calculateTotalOrderPrice(orderPrice, shippingFee);
+
+  const handleOrder = () => {
+    navigate("/result", {
+      state: {
+        checkedItems,
+      },
+    });
+  };
+
   return (
     <div>
       <div> 주문 금액: {orderPrice.toLocaleString()}원</div>
       <div> 배송비: {shippingFee.toLocaleString()}원</div>
       <div>총 결제 금액: {totalOrderPrice.toLocaleString()}원</div>
+      <button onClick={handleOrder} disabled={checkedItems.length === 0}>
+        주문확인
+      </button>
     </div>
   );
 };
