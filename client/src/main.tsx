4404df79f5bfae30cb333ff614878df79d804206
iftype <conditionaltype@gmail.com>
2026-06-22 05:42:56 +0900

    fix: MSW제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,19 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-async function enableMocking() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import("./test/mocks/browser");
-    return worker.start({
-      onUnhandledRequest: "bypass",
-    });
-  }
-}
+// async function enableMocking() {
+//   if (import.meta.env.DEV) {
+//     const { worker } = await import("./test/mocks/browser");
+//     return worker.start({
+//       onUnhandledRequest: "bypass",
+//     });
+//   }
+// }
 
-enableMocking().then(() => {
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-});
+// enableMocking().then(() => {
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
+// });
