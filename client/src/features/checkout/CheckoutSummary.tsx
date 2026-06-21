d5f53071c47d3905fe50310e6df8a1bb7fc5bf0a
iftype <conditionaltype@gmail.com>
2026-06-22 04:13:16 +0900

    feat: checkoutSummary 컴포넌트 구현

--- client/src/features/checkout/CheckoutSummary.tsx
+++ client/src/features/checkout/CheckoutSummary.tsx
@@ -1,21 +1,21 @@
 import { SummaryContainer, SummaryItem } from "../../shared/Summary";
 
 interface CheckoutSummaryProps {
-  orderPrice: string;
-  shippingFee: string;
-  discountPrice: string;
-  totalOrderPrice: string;
+  order_price: number;
+  delivery_price: number;
+  dicount_price: number;
+  total_price: number;
 }
 
 export const CheckoutSummary = (props: CheckoutSummaryProps) => {
-  const { orderPrice, shippingFee, discountPrice, totalOrderPrice } = props;
+  const { order_price, dicount_price, delivery_price, total_price } = props;
 
   return (
     <SummaryContainer>
-      <SummaryItem title="주문 금액" content={`${orderPrice.toLocaleString()}원`} />
-      <SummaryItem title="쿠폰 할인 금액" content={`${discountPrice.toLocaleString()}원`} />
-      <SummaryItem title="배송비" content={`${shippingFee.toLocaleString()}원`} />
-      <SummaryItem title="총 결제금액" content={`${totalOrderPrice.toLocaleString()}원`} />
+      <SummaryItem title="주문 금액" content={`${order_price.toLocaleString()}원`} />
+      <SummaryItem title="쿠폰 할인 금액" content={`${dicount_price.toLocaleString()}원`} />
+      <SummaryItem title="배송비" content={`${delivery_price.toLocaleString()}원`} />
+      <SummaryItem title="총 결제금액" content={`${total_price.toLocaleString()}원`} />
     </SummaryContainer>
   );
 };
